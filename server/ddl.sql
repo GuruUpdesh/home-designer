5e31faf1a3398ec886bcf6669f4d11affff56a3d
Excalibur0331 <eli1838459978@gmail.com>
2022-02-24 17:13:34 -0800

    finsihed ddl and dml.sql

--- server/ddl.sql
+++ server/ddl.sql
@@ -13,20 +13,20 @@ CREATE TABLE Clients (
     PRIMARY KEY (`clientID`)
 );
 
+
+
 CREATE TABLE Addresses (
     addressID int (255) auto_increment unique not Null,
-    zipCode varchar(5) not Null, 
-    state varchar(255) not Null, 
-    city varchar(255) not Null,
-    street int (11) unique not Null , 
+    address varchar(255) not Null,
     isComplete boolean not null default 0,
     dateStarted datetime not Null,
     dateComplete datetime,
-    cid int,
-    PRIMARY KEY (`addressID`),
-    KEY `Clients_ibfk_1` (`cid`),
-    CONSTRAINT `Clients_ibfk_1` foreign key (`cid`) REFERENCES `Clients` (`clientID`)
+    cID int,
+    PRIMARY KEY (`addressID`), 
+    KEY `Clients_ibfk_1` (`cID`),
+    CONSTRAINT `Clients_ibfk_1` foreign key (`cID`) REFERENCES `Clients` (`clientID`)
 );
+
 CREATE TABLE Employees (
     employeeID int (11) auto_increment unique not Null,
     name varchar(255) not Null, 
@@ -35,6 +35,7 @@ CREATE TABLE Employees (
     isCurrentEmployee boolean not null default 1,
     PRIMARY KEY (`employeeID`)
 );
+
 CREATE TABLE Projects (
     projectID int (11) auto_increment unique not Null,
     name varchar(800 ) not Null, 
@@ -47,30 +48,30 @@ CREATE TABLE Projects (
     KEY `Projects_ibfk_1` (`aid`),
     CONSTRAINT `Projects_ibfk_1` foreign key (`aid`) REFERENCES `Addresses` (`addressID`)
 );
+
 CREATE TABLE Billing_Hours (
     billingHoursID int (11) auto_increment unique not Null,
     timeIn varchar(255 ) not Null, 
     timeOut varchar(255) not Null,
     descriptionOfWork text,
     eid int,
-    pid int UNIQUE,
+    pID int UNIQUE,
     PRIMARY KEY (`billingHoursID`),
-    KEY `Billing_Hours_ibfk_1` (`eid`),
-    KEY `Billing_Hours_ibfk_2` (`pid`),
-    CONSTRAINT `Billing_Hours_ibfk_1` foreign key (`eid`) REFERENCES `Employees` (`employeeID`),
-    CONSTRAINT `Billing_Hours_ibfk_2` foreign key (`pid`) REFERENCES `Projects` (`projectID`)
+    KEY `Billing_Hours_ibfk_1` (`eID`),
+    KEY `Billing_Hours_ibfk_2` (`pID`),
+    CONSTRAINT `Billing_Hours_ibfk_1` foreign key (`eID`) REFERENCES `Employees` (`employeeID`),
+    CONSTRAINT `Billing_Hours_ibfk_2` foreign key (`pID`) REFERENCES `Projects` (`projectID`)
 );
-
 
 CREATE TABLE Projects_Employees (
     projectEmployeeID int (11) auto_increment unique not Null,
-	pid int,
-    eid int,
+	pID int,
+    eID int,
     PRIMARY KEY (`projectEmployeeID`),
-    KEY `Projects_Employees_ibfk_1` (`pid`),
-    KEY `Projects_Employees_ibfk_2` (`eid`),
-    CONSTRAINT `Projects_Employees_ibfk_1` foreign key (`pid`) REFERENCES `Projects` (`projectID`),
-    CONSTRAINT `Projects_Employees_ibfk_2` foreign key (`eid`) REFERENCES `Employees` (`employeeID`)
+    KEY `Projects_Employees_ibfk_1` (`pID`),
+    KEY `Projects_Employees_ibfk_2` (`eID`),
+    CONSTRAINT `Projects_Employees_ibfk_1` foreign key (`pID`) REFERENCES `Projects` (`projectID`),
+    CONSTRAINT `Projects_Employees_ibfk_2` foreign key (`eID`) REFERENCES `Employees` (`employeeID`)
 );
 
 
@@ -78,22 +79,16 @@ INSERT INTO Clients (name,email,phone) values ('jin yang', 'smiths@hello.com', '
 INSERT INTO Clients (name,email,phone) values ('jack barker', 'miths@hello.com', '31');
 
 
-INSERT INTO Addresses (`zipCode`,`state`,`city`,`street`,`isComplete`,`dateStarted`,`dateComplete`,`cid`) values (
-    '32221',
-    'FL',
-    'Jacksonville',
-    'second street',
+INSERT INTO Addresses (`address`,`isComplete`,`dateStarted`,`dateComplete`,`cID`) values (
+    '32221 FL Jacksonville second street',
     'false',
     DATE '2015-11-17',
     DATE '2015-12-17',
      (SELECT clientID FROM Clients WHERE name = 'jin yang')
 );
 
-INSERT INTO Addresses (`zipCode`,`state`,`city`,`street`,`isComplete`,`dateStarted`,`dateComplete`,`cid`) values (
-    '32222',
-    'FL',
-    'Jacksonville2',
-    '3HG4',
+INSERT INTO Addresses (`address`,`isComplete`,`dateStarted`,`dateComplete`,`cID`) values (
+    '32222 FL Jacksonville2 3HG4',
     'false',
     DATE '2015-11-17',
     DATE '2015-12-17',
@@ -119,7 +114,7 @@ INSERT INTO Projects (`name`,`dateStarted`,`dateComplete`,`isComplete`,`projectD
     DATE '2019-12-17',
     'false',
     'new stadium',
-     (SELECT addressID FROM Addresses WHERE street = 'second street')
+     (SELECT addressID FROM Addresses WHERE address like 'second street')
 );
 INSERT INTO Projects (`name`,`dateStarted`,`dateComplete`,`isComplete`,`projectDescription`,`aid`) values (
     'your moms kitchen',
@@ -127,7 +122,7 @@ INSERT INTO Projects (`name`,`dateStarted`,`dateComplete`,`isComplete`,`projectD
     DATE '2019-12-17',
     'false',
     'renovate kitchen',
-     (SELECT addressID FROM Addresses WHERE street = 'second street')
+     (SELECT addressID FROM Addresses WHERE address like 'FL')
 );
 
 INSERT INTO Billing_Hours (`timeIn`,`timeOut`,`descriptionOfWork`,`eid`,`pid`) values (
